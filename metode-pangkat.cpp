3fb7c59beac67b1d9efa932afd5b807b8ec191ba
ita147 <62578693+ita147@users.noreply.github.com>
2020-03-31 06:43:11 +0700

    Create metode-pangkat.cpp

---
+++ metode-pangkat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+double hasil;
+
+void pangkat(double a,double b){
+	hasil=pow(a,b);
+	cout<<"hasil "<<a<<"^"<<b<<"="<<hasil;
+}
+
+main(){
+	int pilih;double a; double b;
+	cout<<"masukkan angka1 :";cin>>a;
+	cout<<"masukkan angka2 :";cin>>b;
+			pangkat(a,b);
+}
